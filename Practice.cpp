89321c251b7f0d930b799b2c9c29e49fb03939ee
Uriel Gomez <Brokenlevenheart@gmail.com>
2025-02-28 22:52:25 -0600

    Hipotenusa en c++ (Estudio personal)
    Si, subir cosas porque si me gusta, me recuerdan no soy tan tonto

---
+++ Practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <cmath> 
+
+
+int main(){
+
+    double a; 
+    double b; 
+    double c;
+//Entrada dato "a"
+    std::cout<< "enter data A"; 
+    std::cin >> a; 
+//Entrada dato "a"
+    std::cout<< "enter data b";
+    std::cin >>b; 
+
+    a = pow(a , 2); 
+    b = pow (b, 2);
+
+//calculos por cmath
+
+    c = sqrt(a+b);
+
+    std::cout<<"la hipotenusa es igual a: " << c; 
+    return 0; 
+}
