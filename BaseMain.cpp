9144a87e74dbdab2e0d6eeef8971488a211b8eea
Uriel Gomez <Brokenlevenheart@gmail.com>
2025-03-04 18:15:50 -0600

    Create basede-BaseMain.cpp

---
+++ BaseMain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+void 
+
+#funcion de variable #1
+int main2(){
+int a=;
+int b=;
+vector(a<b)::vector (a=2)
+}
+
+#funcionn de variable del Main 2
+int main1(){
+std::string main;
+std::cout <<"entrada";
+return main2
+}
