96c4efa14c1eb894afa10ba3853bb24a2731c076
Uriel Gomez <Brokenlevenheart@gmail.com>
2025-03-19 14:11:04 -0600

    Update Main.py

--- Proyecto Intro a informatica/Main.py
+++ Proyecto Intro a informatica/Main.py
@@ -1,33 +1,66 @@
-#defUIser
-User = "Empresagenerica.VeigaMarses@musica.com".lower()
-UserName = "Veiga Marses"
-Passcode = "EmpleadoAcceso"
+#Temporal Users
 
-#def userinput 
-userinput = input ("Ingrese su usuario").lower()
-passcodeinput = input("Ingrese su contraseña")
+########################################## User temporal data info
+User = "Empresagenerica@gmail.com".lower()
+passcode = "EmpresaGeneica"
+#####################################################
+# menu                                              #
+#####################################################
+def menu():
+    print("\n Bienvenido a (inserte empresa generica)")
+    print("\n si desea inicias secion precione 1")
+    print("\n si desea salir precione 2")
 
-#Definventario (Todo lo relacionado al inventario)
-Laptops = 2
-Mouses = 4
-Teclados = 6
-Audífonos  = 8
-computadoras_de_escritorio = 10
+######################################################
+# stock test Actual                                  #
+######################################################
+LAPTOPS_CANTIDAD = 10
+MOUSES_CANTIDAD = 15
+TECLADOS_CANTIDAD = 15
+AUDIFONOS_CANTIDAD = 12
+COMPUTADORAS_ESCRITORIO_CANTIDAD = 8
+################################################
+# funcion main, aqui deberia estar alojado todo#
+################################################
+def main():
+    while  True:
+        menu()
+        try:
+            ######### user decition
+            Eleccion = int(input("Esperando instruccion"))
+            if Eleccion == 1:
+                Userinput = input("Por favor ingrese su usuario").lower()
+                #validacion de usuario
+                if Userinput == User:
+                    inputpasscode = input("Por favor ingrese su contraseña")
+                    #contraseña
+                    if inputpasscode == passcode:
+                        print("Bienvenido usuario")
+                    else:
+                        print("Contraseña incorrecta")
+                else:
+                    print("Usuario no valido")
+            if Eleccion == 2:
+                print("Apagando Sistema")
+                break
+        except ValueError:
+            print("Error en la introduccion de datos")
+            break
+    ############################################################
+    #funcion inventario, aqui deberia estar alojado la central #
+    ############################################################
 
-#funcion
-def iniciosesion():
-    print("Bienvenido al sistema de facturacion de la empres (Inserte nombre generico aqui) por favor introdusca sus datos")
-    userinput()
+def inventario_main():
+    print("Inventario actual:")
+print(f"Laptops: {LAPTOPS_CANTIDAD}")
+print(f"Mouses:  {MOUSES_CANTIDAD}")
+print(f"Teclados: {TECLADOS_CANTIDAD}")
+print(f"Audifonos: {AUDIFONOS_CANTIDAD}")
+print(f"Computadoras de escritotio: {COMPUTADORAS_ESCRITORIO_CANTIDAD}")
+# deberia poder introducir la parte Fatalis, Fatalis deberia poder arreglarla en caso de error en el citado.
+#notas no importantes (OSea putiar al lenguaje (si esto esta en las notas finales de entrega recuerden, solo es la rabia de un programador))
+#Me explican porque mierda no puedo importar un importe?
 
-class inventario():
 
-    def stock(self):
-        print("Este es el inventario actual")
-        userinput
-        if userinput == User:
-            print(F"usuario{UserName}")
-
- 
-def userconsole():
-    while True:
-        iniciosesion()
+#nota para Miaurziso:
+#Falta conectar archivos y refactorio.
